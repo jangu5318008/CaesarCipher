5046f72b96a62b69110bbbc93502d5ee7db60ec2
jangu5318008 <jasonnguyen185@gmail.com>
2026-04-24 12:32:04 -0700

    empty

---
+++ CaesarCipher/CaesarCipher.cpp
@@ -0,0 +1,55 @@
+#include "CaesarCipher.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CaesarCipher {
+public:
+    CaesarCipher(int _key);
+    string encrypt(string text);
+    string decrypt(string text);
+private:
+    int key;
+};
+
+
+CaesarCipher::CaesarCipher(int _key) {
+    key = (_key % 26);
+    while (key < 0) {
+        key = key + 26; 
+    }
+
+}
+
+
+string CaesarCipher::encrypt(string text) {
+    for (int i = 0; i < text.size(); i++) {
+        if (isalpha(text[i])) {
+            if (islower(text[i])) {
+               text[i] = (text[i] - 'a' + key) % 26 + 'a'; 
+            }
+            else if (isupper(text[i])) {
+                text[i] = (text[i] - 'A' + key) % 26 + 'A';
+            }
+        }
+    }
+    return text;
+}
+string CaesarCipher::decrypt(string text) {
+    for (int i = 0; i < text.size(); i++) {
+        if (isalpha(text[i])) {
+            if (islower(text[i])) {
+                text[i] = (text[i] - 'a' + 26 - key) % 26 + 'a';
+            }
+            else if (isupper(text[i])) {
+                text[i] = (text[i] - 'A' + 26 - key) % 26 + 'A';
+            }
+        }
+    }
+    return text;
+}
+
+
+
+
--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include "CaesarCipher.h"   
+#include <fstream>
 
-
+using namespace std;
 
 int main() {
 
+    int n;
+    string text;
+    string input;
 
+
+    while (true) {
+        cout << "Input each line of text, followed by enter. Input 'q' to quit";
+        getline(cin, input);
+        if (input == "q") {
+            break;
+        }
+        else {
+            text = input + '\n'; 
+
+        }
+    }
+            while (true) {
+                cout << "Input the key (integer): ";
+                cin >> n;
+                if (n < 0) {
+                    cout << "Invalid key. Please enter a non-negative integer." << endl;
+                    cin.clear();
+                    //will only ignore up to 10k characters
+                    cin.ignore(10000, '\n');
+
+                }
+
+            }
+
+            CaesarCipher cipher(n);
+            string encrypted = cipher.encrypt(input);
+            cout << "Encrypted: " << encrypted << endl;
+            string decrypted = cipher.decrypt(encrypted);
+            cout << "Decrypted: " << decrypted << endl;
 
 
 
